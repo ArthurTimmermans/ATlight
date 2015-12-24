0a1e4f9e962cedb5b54fa00672e794619abded9e
Arthur Timmermans <arthur.timmermans@icloud.com>
2015-12-24 21:17:06 +0100

    Setup led and button pins

--- ATlight/ATlight.ino
+++ ATlight/ATlight.ino
@@ -1,7 +1,26 @@
-void setup() {
+
+  int ledRed = 3;
+  int ledGreen = 4;
+  int ledBlue = 5;
+
+  int butUp = 7;
+  int butMid = 8;
+  int butDown = 9;
+
+  
+void setup(){ 
+
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+
+  pinMode(butUp, INPUT);
+  pinMode(butMid, INPUT);
+  pinMode(butDown, INPUT);
+
   
 }
 
-void loop() {
-  
+void loop(){
+
 }
