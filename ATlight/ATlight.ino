93134e07b4f4927425e22e58c83296fd621d9336
Arthur Timmermans <arthur.timmermans@icloud.com>
2016-01-20 22:06:54 +0100

    Finished base menu system(for so far). Sub menu started.

--- ATlight/ATlight.ino
+++ ATlight/ATlight.ino
@@ -6,9 +6,19 @@
   int ledRed = 22;
   int ledGreen =23;
 
-  int butUp = 7;
-  int butMid = 8;
-  int butDown = 9;
+  int butUp = 26;
+  int butMid = 25;
+  int butDown = 24;
+
+  int mode = 0;
+  
+  int menuLevel = 0;
+  
+  int menuUpLast = 0;
+  int menuUpState = 0;
+
+  int menuDownLast = 0;
+  int menuDownState = 0;
 
   // LCD setup
   #define I2C_ADDR 0x27
@@ -62,23 +72,71 @@ void setup(){
 }
 
 void menu(){
-
-  switch(menuItem){
-    case 0:
-      menuItem++;
-      //menuName = "Addressing";
-      break;
-  }
   lcd.print("Home");
   lcd.setCursor(0,1);
-  lcd.print("> Addressing");
+  if(mode == 0){
+    switch(menuLevel){
+      case 0:
+        lcd.print("> ADDRESSING");
+        if(digitalRead(butMid) == HIGH){
+          mode = 1;
+        }
+        break;
+      case 1:
+        lcd.print("> SETTINGS");
+        if(digitalRead(butMid) == HIGH){
+          mode = 2;
+        }
+        break;
+      case 2:
+        lcd.print("> SHUTDOWN");
+        if(digitalRead(butMid) == HIGH){
+          mode = 3;
+        }
+        break;
+      default:
+        if(menuLevel == -1){
+          menuLevel = 2;
+        }else{
+          menuLevel = 0;
+        }
+    }
+
+    switch(mode){
+      case 1:
+        lcd.print("test");
+        break;
+    }
+  }
+  
   lcd.home();
 }
 
 void loop(){
-
+Serial.println(menuLevel);
 menu();
 
+menuUpState = digitalRead(butUp);
+if(menuUpState != menuUpLast){
+  if(menuUpState == HIGH){
+      menuLevel++;
+      lcd.clear();
+  }
+  delay(50);
+}
+menuUpLast = menuUpState;
+
+
+menuDownState = digitalRead(butDown);
+if(menuDownState != menuDownLast){
+  if(menuDownState == HIGH){
+    menuLevel--;
+    lcd.clear();
+  }
+  delay(50);
+}
+menuDownLast = menuDownState;
 
   
 }
+// http://www.instructables.com/id/I2C-between-Arduinos/
